60f322330ffe0e1d60e03a33c5a9952d93762af4
RomanDZ <zaderetskyiroman@gmail.com>
2020-10-28 19:28:26 +0200

    Homework1

---
+++ homework.js
@@ -0,0 +1,16 @@
+'use strict';
+
+let money = confirm("Ваш бюджет на месяц?");
+ console.log(money);
+ let time = confirm("Введите дату в формате YYYY-MM-DD");
+ console.log(time);
+
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses:  ,
+    optionalExpenses: ,
+    income: [] ,
+    saving: false
+};
