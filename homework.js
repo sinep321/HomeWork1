878583975d1d869bee1026a7a09757a130fde48a
RomanDZ <zaderetskyiroman@gmail.com>
2020-10-29 11:37:55 +0200

    homeWorkDone

--- homework.js
+++ homework.js
@@ -1,16 +1,25 @@
 'use strict';
 
-let money = confirm("Ваш бюджет на месяц?");
- console.log(money);
- let time = confirm("Введите дату в формате YYYY-MM-DD");
- console.log(time);
+let money = prompt("Ваш бюджет на месяц?", "");
+console.log(money);
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+console.log(time);
 
+let expenses1 = prompt("Введите обязательную статью расходов в этом месяце");
+console.log(expenses1);
+let expenses2 = prompt("Во сколько обойдется?");
+console.log(expenses2);
 
 let appData = {
     budget: money,
     timeData: time,
-    expenses:  ,
-    optionalExpenses: ,
-    income: [] ,
+    expenses: {
+        expenses1: expenses2
+    },
+    // optionalExpenses: ,
+    // income: [] ,
     saving: false
 };
+
+let budgetDay = ("Ваш бюджет на день: " + appData.budget / 30);
+alert(budgetDay);
